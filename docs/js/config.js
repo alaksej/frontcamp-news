4152d99505390ccc311945ce0d0804312b412334
Aliaksei Yankou <yankouav@gmail.com>
2018-11-11 23:21:42 +0300

    add App class and refactor config

--- docs/js/config.js
+++ docs/js/config.js
@@ -1,9 +1,20 @@
 export const apiKey = 'd63e3d1d1622450aaf814fae749afce1';
-
-export const sourcesConfig = new Map([
-  ['1', { displayName: 'Top headlines from BBC News', config: { action: 'topHeadlines', params: { sources: 'bbc-news' } } }],
-  ['2', { displayName: 'Articles about Bitcoin', config: { action: 'everything', params: { q: 'bitcoin' } } }],
-  ['3', { displayName: 'Top sports headlines', config: { action: 'topHeadlines', params: { category: 'sport' } } }],
-]);
-
 export const genericNewsLogoPath = './images/generic_news_logo.png';
+
+export class SourcesConfig {
+  constructor() {
+    this._sourcesConfig = new Map([
+      ['1', { displayName: 'Top headlines from BBC News', urlConfig: { action: 'topHeadlines', params: { sources: 'bbc-news' } } }],
+      ['2', { displayName: 'Articles about Bitcoin', urlConfig: { action: 'everything', params: { q: 'bitcoin' } } }],
+      ['3', { displayName: 'Top sports headlines', urlConfig: { action: 'topHeadlines', params: { category: 'sport' } } }],
+    ]);
+  }
+
+  getSearchPanelOptions() {
+    return Array.from(this._sourcesConfig).map(source => ({ value: source[0], displayName: source[1].displayName }));
+  }
+
+  getUrlConfig(sourceKey) {
+    return this._sourcesConfig.get(sourceKey).urlConfig;
+  }
+}
--- docs/js/main.js
+++ docs/js/main.js
@@ -1,41 +1,50 @@
-import { apiKey, sourcesConfig, genericNewsLogoPath } from './config.js';
+import { apiKey, SourcesConfig, genericNewsLogoPath } from './config.js';
 import { EventEmitter } from './event-emitter.js';
 import { DOMHelper, isIterable } from './utls.js';
 import { NewsAPI } from './news-api.js';
 
 /** Initialize the application here */
-document.addEventListener('DOMContentLoaded', () => {
-  const sources = Array.from(sourcesConfig).map(source => ({ value: source[0], displayName: source[1].displayName })),
-    newsApi = new NewsAPI(apiKey),
-    searchPanel = new SearchPanel({ sources }),
-    newsList = new NewsList();
+document.addEventListener('DOMContentLoaded', () => new App());
 
-  newsList.text = `Click 'Go' to get some news!`;
+/** The main application class */
+class App {
+  constructor() {
+    this._sourcesConfig = new SourcesConfig();
+    this._newsApi = new NewsAPI(apiKey);
+    this._searchPanel = new SearchPanel({ sources: this._sourcesConfig.getSearchPanelOptions() });
+    this._newsList = new NewsList();
 
-  searchPanel.submitClick.subscribe(async ({ source: sourceKey, page }) => {
-    searchPanel.disableSubmit();
-    newsList.text = 'Loading...';
-    const { action, params } = sourcesConfig.get(sourceKey).config;
+    this._newsList.text = `Click 'Go' to get some news!`;
+
+    this._searchPanel.submitClick.subscribe(async ({ source: sourceKey, page }) => {
+      const { action, params } = this._sourcesConfig.getUrlConfig(sourceKey);
+      await this.loadNews(action, { ...params, page });
+    });
+  }
+
+  async loadNews(action, params) {
+    this._searchPanel.disableSubmit();
+    this._newsList.text = 'Loading...';
 
     let result;
     try {
-      result = await newsApi[action]({ ...params, page });
+      result = await this._newsApi[action](params);
     } catch {
-      newsList.text = 'Oops, something went wrong. Maybe the page number is too big?';
+      this._newsList.text = 'Oops, something went wrong. Maybe the page number is too big?';
     }
 
     if (result) {
       const articles = result.articles;
       if (articles && articles.length) {
-        newsList.articles = articles;
+        this._newsList.articles = articles;
       } else {
-        newsList.text = `Nothing's found. Try changing the channel of page number.`;
+        this._newsList.text = `Nothing's found. Try changing the channel of page number.`;
       }
     }
 
-    searchPanel.enableSubmit();
-  });
-});
+    this._searchPanel.enableSubmit();
+  }
+}
 
 /** Displays the list of the news articles */
 class NewsList {
