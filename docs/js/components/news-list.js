4d243d03fd13a2a177b50d98478a29b7c957cf7b
Aliaksei Yankou <yankouav@gmail.com>
2018-11-14 22:44:55 +0300

    add placeholder while the images are loading

--- docs/js/components/news-list.js
+++ docs/js/components/news-list.js
@@ -25,13 +25,14 @@ export class NewsList {
     if (isIterable(articles)) {
       this._newsListContainer.innerHTML = Array.from(articles, article => this._createCardEl(article)).join('');
     }
+    document.querySelectorAll('.card-image img').forEach(image => image.onload = () => image.parentElement.classList.remove('loading'));
   }
 
   _createCardEl(article) {
     return `
       <div class="card">
         <a class="card-link" href="${article.url}" target="_blank">
-          <div class="card-image">
+          <div class="card-image loading">
             <img src="${article.urlToImage || genericNewsLogoPath}" alt="${article.title}">
           </div>
           <div class="card-text">
