23f841f7af7a9c4184c0e36a964b1ec271131ce3
Aliaksei Yankou <yankouav@gmail.com>
2018-12-02 19:37:08 +0300

    apply IE fix for proxy

--- src/app/common/http/http-client.proxy.js
+++ src/app/common/http/http-client.proxy.js
@@ -3,13 +3,23 @@ import { HttpClient } from './http-client.js';
 export class HttpClientProxy {
   constructor() {
     const handlers = {
-      get(target, key) {
-        const originalMethod = target[key];
+      get(target, key, context) {
+        const propertyValue = target[key];
+
+        /** IE fix: without this check, app breaks in IE. 
+         * With it the requests are not logged but at least 
+         * all other features work.
+         * Is it because of partial Proxy polyfilling? */
+        if (typeof propertyValue !== 'function') {
+          return propertyValue;
+        }
+
         return function (...args) {
           console.info(`${key} ${JSON.stringify(args)}`);
-          return originalMethod.apply(target, args);
+          return propertyValue.apply(target, args);
         };
       },
+
     };
     const pobj = new Proxy(new HttpClient(), handlers);
     return pobj;
